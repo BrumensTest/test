dbd40f2b21ff8a6d9b8937576a840f928d8d9b74
BrumensTest <107815229+BrumensTest@users.noreply.github.com>
2022-06-19 22:59:52 +0100

    Update python_test.py

--- python_test.py
+++ python_test.py
@@ -2,7 +2,9 @@
 
 saltOne = 12345
 saltTwo = "secrethere"
-  
+variable_secret="5f4dcc3b5aa765d61d8327deb882cf99"
+
+
 #Credentials
 username = "admin" 
 password = "p4$$w0rd"
@@ -18,3 +20,7 @@ hereisthevariable="5f4dcc3b5aa765d61d8327deb882cf99"+saltTwo
 token=str("5f4dcc3b5aa765d61d8327deb882cf99")
 token=str("5f4dcc3b5aa765d61d8327deb882cf99"+saltOne)
 token="5f4dcc3b5aa765d61d8327deb882cf99"+saltTwo
+
+token=("5f4dcc3b5aa765d61d8327deb882cf99"+saltTwo)
+token=saltTwo+"5f4dcc3b5aa765d61d8327deb882cf99"
+token=variable_secret
