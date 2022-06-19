8e24335ec8ca1ba0774f5b97c161ab9b52319f3f
BrumensTest <107815229+BrumensTest@users.noreply.github.com>
2022-06-19 23:17:47 +0100

    Update python_test.py

--- python_test.py
+++ python_test.py
@@ -28,12 +28,11 @@ token=variable_secret
 
 
 #Zoom api key detect:
-ZOOM_AUTH_SECRET="9NeGUzYkSjRupIF5pt93m2D1w17fOjI8"
-
+ZOOM_AUTH_SECRET="salt123"+"9NeGUzYkSjRupIF5pt93m2D1w17fOjI8"
+ZOOM_AUTH_SECRET=12345+"9NeGUzYkSjRupIF5pt93m2D1w17fOjI812345"
 
 if qwerty == "9NeGUzYkSjRupIF5pt93m2D1w17fOjI8":
-  print("logged in")
-  
+  print("logged in") 
  
 if 1==1 || qwerty == "9NeGUzYkSjRupIF5pt93m2D1w17fOjI8":
   print("logged in")
