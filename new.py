790d07d4c2d564a3b26ac1e799377eddf614dd4d
BrumensTest <107815229+BrumensTest@users.noreply.github.com>
2022-06-19 23:22:12 +0100

    Create new.py

---
+++ new.py
@@ -0,0 +1,23 @@
+#!/usr/share/python3
+
+print("starting process")
+
+ZOOM_AUTH_SECRET=12345+"9NeGUzYkSjRupIF5pt93m2D1w17fOjI812345"
+
+if qwerty == "9NeGUzYkSjRupIF5pt93m2D1w17fOjI8":
+  print("logged in") 
+ 
+if 1==1 || qwerty == "9NeGUzYkSjRupIF5pt93m2D1w17fOjI8":
+  print("logged in")
+  
+  
+if 1==1||zoom_auth_secret == "9NeGUzYkSjRupIF5pt93m2D1w17fOjI8":
+  print("this worked")
+  
+    
+if 1==1 || zoom_auth_secret == "9NeGUzYkSjRupIF5pt93m2D1w17fOjI8" && admin == true:
+  print("this worked")
+
+#zoom_auth_secret 9NeGUzYkSjRupIF5pt93m2D1w17fOjI8
+
+print("end of process")
