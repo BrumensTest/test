d8cd20e0048c3f5574c79269a4444f3665b06a5a
BrumensTest <107815229+BrumensTest@users.noreply.github.com>
2022-06-19 23:20:38 +0100

    Update python_test.py

--- python_test.py
+++ python_test.py
@@ -1,34 +1,7 @@
 #!/usr/share/python3
 
-saltOne = 12345
-saltTwo = "secrethere"
-variable_secret="5f4dcc3b5aa765d61d8327deb882cf99"
+print("starting process")
 
-
-#Credentials
-username = "admin" 
-password = "p4$$w0rd"
-hereisthevariable=str("5f4dcc3b5aa765d61d8327deb882cf99")
-hereisthevariable="5f4dcc3b5aa765d61d8327deb882cf99"
-
-
-
-hereisthevariable=str("5f4dcc3b5aa765d61d8327deb882cf99"+saltOne)
-hereisthevariable="5f4dcc3b5aa765d61d8327deb882cf99"+saltTwo
-
-
-token=str("5f4dcc3b5aa765d61d8327deb882cf99")
-token=str("5f4dcc3b5aa765d61d8327deb882cf99"+saltOne)
-token="5f4dcc3b5aa765d61d8327deb882cf99"+saltTwo
-
-token=("5f4dcc3b5aa765d61d8327deb882cf99"+saltTwo)
-token=saltTwo+"5f4dcc3b5aa765d61d8327deb882cf99"
-token=variable_secret
-
-
-
-#Zoom api key detect:
-ZOOM_AUTH_SECRET="salt123"+"9NeGUzYkSjRupIF5pt93m2D1w17fOjI8"
 ZOOM_AUTH_SECRET=12345+"9NeGUzYkSjRupIF5pt93m2D1w17fOjI812345"
 
 if qwerty == "9NeGUzYkSjRupIF5pt93m2D1w17fOjI8":
@@ -39,8 +12,12 @@ if 1==1 || qwerty == "9NeGUzYkSjRupIF5pt93m2D1w17fOjI8":
   
   
 if 1==1||zoom_auth_secret == "9NeGUzYkSjRupIF5pt93m2D1w17fOjI8":
-  print("logged in")
+  print("this worked")
   
     
 if 1==1 || zoom_auth_secret == "9NeGUzYkSjRupIF5pt93m2D1w17fOjI8" && admin == true:
-  print("logged in")
+  print("this worked")
+
+#zoom_auth_secret 9NeGUzYkSjRupIF5pt93m2D1w17fOjI8
+
+print("end of process")
